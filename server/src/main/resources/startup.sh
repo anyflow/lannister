8c1c6fbb05442499e0a4657f37146cf0fe05c726
Park Hyunjeong <anyflow@gmail.com>
2016-09-16 14:43:26 +0900

    main jar file moved into lib folder / startup script updated

--- server/src/main/resources/startup.sh
+++ server/src/main/resources/startup.sh
@@ -1,11 +1,39 @@
 #!/bin/sh
 
+echo 'Boostrapping Lannister started'
+
 cd ${lannister.home}
 
-tail -f -n0 ${log.path}/output.log &
+echo 'current directory :'
+pwd
+
+echo 'Prepare log directory & file...'
+LOG_DIRECTORY="./logs"
+if [ -d "$LOG_DIRECTORY" ];
+then
+  echo $LOG_DIRECTORY' found'
+else
+  mkdir $LOG_DIRECTORY
+  echo $LOG_DIRECTORY' created'
+fi
+
+LOG_FILE="./logs/output.log"
+if [ -f "$LOG_FILE" ];
+then
+  echo $LOG_FILE' found'
+else
+  touch $LOG_FILE
+  echo $LOG_FILE' created'
+fi
+
+echo 'Monitoring '$LOG_FILE'...'
+tail -f -n0 $LOG_FILE &
 TAIL_PID=$!
 
-nohup java -Dname=${project.build.finalName} ${project.build.startOption} -jar "${project.build.finalName}.jar" >/dev/null 2>&1 &
+echo 'Execute Lannister execution file...'
+EXECUTE_FILE='./lib/${project.build.finalName}.jar'
+
+nohup java -Dname=${project.build.finalName} ${project.build.startOption} -jar $EXECUTE_FILE >/dev/null 2>&1 &
 
 sleep 10
 
